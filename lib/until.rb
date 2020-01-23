ee2adb0d2c7d2c1e91cf5567fd6b12ab5755a0bb
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-23 02:53:23 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
   levitation_force = 6
+  phrase = wingardium Leviosa
   #your code here
-    
+   loop do
+   10.times  # puts phrase "wingardium Leviosa" 
+     until levitation_force = 10
+   end
 end
 
